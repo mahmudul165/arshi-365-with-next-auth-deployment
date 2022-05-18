3cb0dd0e803d19e291c81bd42c34f4e4b4e17bd1
lamptechs <career@lamptechs.com>
2022-05-18 20:42:09 +0600

    testing image path

--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import SignUp from "/home/sign-up.png";
+import SignUp from "/public/home/Sign-up.png";
 function login() {
   return (
     <div
