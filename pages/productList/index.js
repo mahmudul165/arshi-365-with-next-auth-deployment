3ea4c0d8c97ac16be3887537694614d2c91d70db
unknown <mahmudulh883@gmail.com>
2022-05-19 21:01:07 +0600

    blob image src

--- pages/productList/index.js
+++ pages/productList/index.js
@@ -50,44 +50,42 @@ const Products = () => {
       </h3> */}
       <div className="row    text-center my-2 py-2 ">
         {Products.map((product) => (
-          <>
-            <Link key={product.id} href={`products/${product.id}`}>
-              <div className="col-sm-12 col-md-4 p-2  ">
-                <div className="card border-0 " style={{ width: "22rem" }}>
-                  {/* <Image
+          <Link key={product.id} href={`products/${product.id}`}>
+            <div className="col-sm-12 col-md-4 p-2  ">
+              <div className="card border-0 " style={{ width: "22rem" }}>
+                {/* <Image
                     src="/images/jacket-1.png"
                     alt="E-COMMERCE  products"
                     className="card-img-top  p-2  "
                     width={336}
                     height={336}
                   /> */}
-                  {/* egheherher */}
+                {/* egheherher */}
 
-                  <img
-                    src={product.image_one}
-                    alt="E-COMMERCE  products"
-                    className="card-img-top  p-2  "
-                    width={336}
-                    height={336}
-                  />
-                  <div className="card-body">
-                    <h6 className="card-title fs-6 fw-bolder">
-                      {product.short_description}
-                    </h6>
-                    <p
-                      className="text-center fs-5 fw-bolder "
-                      style={{
-                        color: "#ff8095",
-                        border: 0,
-                      }}
-                    >
-                      ৳{product.price}
-                    </p>
-                  </div>
+                <img
+                  src={`data:image/jpeg;base64,${product.image_one}`}
+                  alt="E-COMMERCE  products"
+                  className="card-img-top  p-2  "
+                  width={336}
+                  height={336}
+                />
+                <div className="card-body">
+                  <h6 className="card-title fs-6 fw-bolder">
+                    {product.short_description}
+                  </h6>
+                  <p
+                    className="text-center fs-5 fw-bolder "
+                    style={{
+                      color: "#ff8095",
+                      border: 0,
+                    }}
+                  >
+                    ৳{product.price}
+                  </p>
                 </div>
               </div>
-            </Link>
-          </>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
