00779c9969cad46ef67f5704d4510f865d96acfc
lamptechs <career@lamptechs.com>
2022-05-31 17:55:58 +0600

    added deploy

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   reactStrictMode: true,
+
   env: {
     GOOGLEID:
       "358604525603-mtn0168km0srsf13irosbk0iirkvekel.apps.googleusercontent.com",
@@ -17,3 +18,16 @@ module.exports = {
   },
 };
 // next.config.js
+// project Name
+// Only letters, digits, and underscores
+// arshi-365-pages-nextauth-deploy-project
+// https://arshi-365-pages-nextauth-deploy-project.vercel.app/api/auth/callback/google
+
+//  NEXTAUTH_URL: "http://localhost:3000",
+
+// GOOGLEID:
+//       "15718943464-1f137o9melpvo03mevc1orf2j2gg7j1a.apps.googleusercontent.com",
+
+//     GOOGLESECRET: "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",
+//     358604525603-mtn0168km0srsf13irosbk0iirkvekel.apps.googleusercontent.com
+//  1019720719307-lpi7ioqh7uhp6462chdfdghpan6k8kf5.apps.googleusercontent.com
